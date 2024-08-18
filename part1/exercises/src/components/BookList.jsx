578df46f3a4b882a436fab91fe50dd4dd500db35
Nubchulubs12 <nick.kuczka@gmail.com>
2024-08-18 18:31:50 -0500

    part one

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,5 +1,5 @@
 export default function BookList() {
-   let pageTitle = "shrug";
+   let pageTitle = "The Greatest";
    let book1 = "google.com";
    let book2 = "";
    let book3 = "";
@@ -7,9 +7,9 @@ export default function BookList() {
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+         <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" />
+         <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" />
+         <img src={book3} alt="The London Seance Society by Sarah Penner" />
+      </div>
    );
 }
